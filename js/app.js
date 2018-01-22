9ecca44207eaf387cec0009f2b63b2aaa9b55c9a
omar fesal <omarfesal4296@gmail.com>
2018-01-22 23:28:39 +0200

    feat: check cards

--- js/app.js
+++ js/app.js
@@ -61,3 +61,47 @@ $(".card").click(function(){
 });
 
 
+/* check cards if equal or not equal*/
+function checkCard(item){
+    
+    arrOfitems.push(item);
+    if(counter===2){
+        counter = 0;
+        let item1 = arrOfitems[0],
+            item2 = arrOfitems[1],
+            item1CassName = item1.childNodes[1].className,
+            item2ClassName = item2.childNodes[1].className;
+
+        if(item1CassName===item2ClassName){
+            $(item1).addClass("match");
+            $(item2).addClass("match");
+            
+        }else{
+            
+            setTimeout(function(){
+                $(item1).addClass("unmatch");
+                $(item1).removeClass("open");
+            },500);
+            
+            setTimeout(function(){
+                $(item2).addClass("unmatch");
+                $(item2).removeClass("open");
+            },500);
+            
+            setTimeout(function(){
+                $(item1).removeClass("unmatch");
+                $(item2).removeClass("unmatch");
+            },1000);
+            
+        }
+        moves++;
+        $(".moves")[0].innerHTML = moves;
+        arrOfitems=[];
+        checkCompleteness();
+        calcStars();
+        
+
+    }
+}
+
+
